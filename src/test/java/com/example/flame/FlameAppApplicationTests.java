38484073b8ce5d9143279f4fa22a08cd0912d401
nmakarov <nmakarov@diasoft.ru>
2022-11-18 20:37:12 +0300

    initial commit

---
+++ src/test/java/com/example/flame/FlameAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlameAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
