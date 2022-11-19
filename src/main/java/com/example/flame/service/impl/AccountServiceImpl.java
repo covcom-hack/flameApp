1964b8a478c1278622f431cca9143b3988dc0c46
maksimpancuk <maksim.panchuk29@gmail.com>
2022-11-19 23:17:29 +0300

    Исправлены ошибки

--- src/main/java/com/example/flame/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/flame/service/impl/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.flame.service.impl;
 import com.example.flame.entity.AccountEntity;
 import com.example.flame.repository.AccountRepository;
 import com.example.flame.service.AccountService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
 
     @Override
-    public Optional<ArrayList<AccountEntity>> getAllByUsername(String username) {
+    public Optional<ArrayList<AccountEntity>> getAllByUsername(@NonNull String username) {
         return accountRepository.getAllByUsername(username);
     }
 }
